834090cb4fed69e3a719ddaaf4ae211785f6e863
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-31 01:53:19 -0700

    Add files via upload

---
+++ different-of types-js/variable-types.js
@@ -0,0 +1,58 @@
+// kivabe typeof diye kag kora hoy java script diye..... konta string konta integer number seita buja jay seta keyword typeof diye buja jay........(Data Type ) 
+
+// anouther 
+var alu = 60;
+var moric = 90;
+aluMoric = alu + moric;
+console.log(aluMoric);
+
+
+
+var tshartPrice = 200;
+
+console.log(typeof tshartPrice);
+
+
+var tshartPrice2 = '2000';
+console.log(typeof tshartPrice2);
+
+
+var tshartPrice3 = 'ABCS';
+console.log(typeof tshartPrice3);
+
+
+var tshartPrice4 =31416;
+console.log(typeof tshartPrice4);
+
+
+var suscribe = true;
+console.log(typeof suscribe);
+
+
+var suscribe =false;
+console.log(typeof suscribe);
+
+
+var isRomantic;
+console.log(typeof isRomantic);
+
+// date type
+
+var today = new Date();
+console.log(typeof today);
+
+
+
+
+
+
+// special edition kivabe float ke integer e niya jay abar integer ke float kora jay ei are ki !! dont worry sakib !!!!!!
+
+
+var firstNumber = 0.1;
+var secondNumber = 0.5;
+var total = firstNumber + secondNumber;
+total = total.toFixed(6);
+console.log(total);
+
+
